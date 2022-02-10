9d000b158320c6f3ccd581baa3a52b085e96344c
rhyst <rhys.turner@wiise.com>
2022-02-10 15:43:06 +1100

    Add: update content

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Rhys Turner'
+export const siteTitle = 'Creative Tech Now: Next.js Sample Website'
 
 export default function Layout({ children, home }) {
   return (
